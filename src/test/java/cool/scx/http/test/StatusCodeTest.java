1a86af27d13d19f9e54f6dc1763f5eb9007d2f59
scx567888 <scx567888@outlook.com>
2025-11-07 16:40:04 +0800

    dev

--- src/test/java/cool/scx/http/test/StatusCodeTest.java
+++ src/test/java/cool/scx/http/test/StatusCodeTest.java
@@ -1,7 +1,7 @@
 package cool.scx.http.test;
 
 import cool.scx.http.status_code.HttpStatusCode;
-import cool.scx.http.status_code.ScxHttpStatusHelper;
+import cool.scx.http.status_code.ScxHttpStatusCodeHelper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,7 +14,7 @@ public class StatusCodeTest {
     @Test
     public static void test1() {
         for (var value : HttpStatusCode.values()) {
-            var reasonPhrase = ScxHttpStatusHelper.getReasonPhrase(value);
+            var reasonPhrase = ScxHttpStatusCodeHelper.getReasonPhrase(value);
             if (reasonPhrase == null) {
                 Assert.fail("reasonPhrase is null");
             }
