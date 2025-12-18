4eceb19771177d9889e54ce477e989512ac7a00b
scx567888 <scx567888@outlook.com>
2025-12-18 12:10:45 +0800

    dev

--- src/main/java/dev/scx/http/media/multi_part/MultiPart.java
+++ src/main/java/dev/scx/http/media/multi_part/MultiPart.java
@@ -1,5 +1,8 @@
 package dev.scx.http.media.multi_part;
 
+import static dev.scx.random.ScxRandom.NUMBER_AND_LETTER;
+import static dev.scx.random.ScxRandom.randomString;
+
 /// MultiPart
 ///
 /// @author scx567888
@@ -11,7 +14,7 @@ public interface MultiPart extends Iterable<MultiPartPart> {
     }
 
     static MultiPartWritable of() {
-        return new MultiPartImpl("ScxBoundary" + MultiPartHelper.randomString(16));
+        return new MultiPartImpl("ScxBoundary" + randomString(16, NUMBER_AND_LETTER));
     }
 
     String boundary();
