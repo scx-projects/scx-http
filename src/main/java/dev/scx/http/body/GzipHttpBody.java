393743634762d8d95df0ea6ae29a3ddae966a02b
scx567888 <scx567888@outlook.com>
2025-12-30 18:13:19 +0800

    dev

--- src/main/java/dev/scx/http/body/GzipHttpBody.java
+++ src/main/java/dev/scx/http/body/GzipHttpBody.java
@@ -32,12 +32,7 @@ public final class GzipHttpBody implements ScxHttpBody {
         }
         //等于 GZIP 我们尝试包装
         if (contentEncoding == GZIP) {
-            try {
-                return ScxIO.gzipByteInput(byteInput);
-            } catch (ScxIOException e) {
-                //原始流有可能并不是一个 合法的 gzip 流 我们抛出异常
-                throw new UnsupportedMediaTypeException(e.getCause());
-            }
+            return ScxIO.gzipByteInput(byteInput);
         } else {// 否则我们不支持这种类型 抛出异常
             throw new UnsupportedMediaTypeException("Unsupported Content-Encoding: " + contentEncoding);
         }
