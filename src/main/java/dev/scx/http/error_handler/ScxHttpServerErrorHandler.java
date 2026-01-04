8d40165f575131fcb741563f00ac200a349eb17a
scx567888 <scx567888@outlook.com>
2026-01-04 15:30:53 +0800

    dev

--- src/main/java/dev/scx/http/error_handler/ScxHttpServerErrorHandler.java
+++ src/main/java/dev/scx/http/error_handler/ScxHttpServerErrorHandler.java
@@ -4,6 +4,10 @@ import dev.scx.http.ScxHttpServerRequest;
 
 /// HTTP 服务器 错误处理器
 ///
+///  实现必须识别 ScxHttpException:
+///
+///  若 throwable instanceof ScxHttpException, 则应使用其 statusCode 生成 HTTP 响应.
+///
 /// @author scx567888
 /// @version 0.0.1
 public interface ScxHttpServerErrorHandler {
