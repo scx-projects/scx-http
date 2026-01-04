391b971afbc24a3af5597adc4bf6cb8cee9c837b
scx567888 <scx567888@outlook.com>
2026-01-04 15:44:28 +0800

    v0.0.6

--- src/main/java/dev/scx/http/body/ScxHttpBody.java
+++ src/main/java/dev/scx/http/body/ScxHttpBody.java
@@ -5,13 +5,10 @@ import dev.scx.http.media.event_stream.ClientEventStream;
 import dev.scx.http.media.file.FileMediaReader;
 import dev.scx.http.media.form_params.FormParams;
 import dev.scx.http.media.multi_part.MultiPartStream;
-import dev.scx.http.media.object.ObjectMediaReader;
 import dev.scx.http.media.string.StringMediaReader;
 import dev.scx.io.ByteInput;
 import dev.scx.io.exception.AlreadyClosedException;
 import dev.scx.io.exception.ScxIOException;
-import dev.scx.node.Node;
-import dev.scx.reflect.TypeReference;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -20,7 +17,6 @@ import static dev.scx.http.media.byte_array.ByteArrayMediaReader.BYTE_ARRAY_MEDI
 import static dev.scx.http.media.event_stream.ClientEventStreamMediaReader.CLIENT_EVENT_STREAM_MEDIA_READER;
 import static dev.scx.http.media.form_params.FormParamsMediaReader.FORM_PARAMS_MEDIA_READER;
 import static dev.scx.http.media.multi_part.MultiPartStreamMediaReader.MULTI_PART_STREAM_MEDIA_READER;
-import static dev.scx.http.media.node.NodeMediaReader.NODE_MEDIA_READER;
 import static dev.scx.http.media.string.StringMediaReader.STRING_MEDIA_READER;
 
 /// ScxHttpBody
@@ -57,18 +53,6 @@ public interface ScxHttpBody {
 
     default MultiPartStream asMultiPart() throws ScxIOException, AlreadyClosedException {
         return as(MULTI_PART_STREAM_MEDIA_READER);
-    }
-
-    default Node asNode() throws ScxIOException, AlreadyClosedException {
-        return as(NODE_MEDIA_READER);
-    }
-
-    default <T> T asObject(Class<T> c) throws ScxIOException, AlreadyClosedException {
-        return as(new ObjectMediaReader<>(c));
-    }
-
-    default <T> T asObject(TypeReference<T> c) throws ScxIOException, AlreadyClosedException {
-        return as(new ObjectMediaReader<>(c));
     }
 
     default ClientEventStream asEventStream() throws ScxIOException, AlreadyClosedException {
