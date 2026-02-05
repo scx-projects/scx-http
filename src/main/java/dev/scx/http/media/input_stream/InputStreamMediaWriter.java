bdd3fd239bc42dc27af929bafcc60bf5c5eb74a0
scx567888 <scx567888@outlook.com>
2026-02-05 13:35:19 +0800

    dev

--- src/main/java/dev/scx/http/media/input_stream/InputStreamMediaWriter.java
+++ src/main/java/dev/scx/http/media/input_stream/InputStreamMediaWriter.java
@@ -14,23 +14,16 @@ import java.io.InputStream;
 
 /// InputStreamMediaWriter
 ///
-/// 设计思路和 ByteInputMediaWriter 保持一致
+/// 默认自动关闭输入流
 ///
 /// @author scx567888
 /// @version 0.0.1
 public final class InputStreamMediaWriter implements MediaWriter {
 
     private final InputStreamByteSupplier inputStreamByteSupplier;
-    private final boolean autoClose;
 
     public InputStreamMediaWriter(InputStream inputStream) {
-        // 直接传输的时候 一般表示 整个输入流已经被用尽了 所以这里我们默认自动关闭输入流.
-        this(inputStream, true);
-    }
-
-    public InputStreamMediaWriter(InputStream inputStream, boolean autoClose) {
         this.inputStreamByteSupplier = new InputStreamByteSupplier(inputStream);
-        this.autoClose = autoClose;
     }
 
     @Override
@@ -46,22 +39,11 @@ public final class InputStreamMediaWriter implements MediaWriter {
 
     @Override
     public void write(ByteOutput byteOutput) throws ScxOutputException, OutputAlreadyClosedException, IOException {
-        try (byteOutput) {
+        try (byteOutput; inputStreamByteSupplier) {
             ScxIO.transferToAll(inputStreamByteSupplier, byteOutput);
         } catch (ScxInputException e) {
             // 这里必然是 IOException 强转安全.
             throw (IOException) e.getCause();
-        }
-        // 不在 finally 中关闭:
-        // 仅当写入流程完整成功(包括 ByteOutput 关闭阶段)且 autoClose=true 时才关闭 inputStream.
-        // 若写入过程中抛出异常, 将保留 inputStream 由调用方自行处理.
-        if (autoClose) {
-            try {
-                inputStreamByteSupplier.close();
-            } catch (ScxInputException e) {
-                // 这里必然是 IOException 强转安全.
-                throw (IOException) e.getCause();
-            }
         }
     }
 
