86c3f2fb7a5f07afc3b7992b7190e5ab16c7a45c
scx567888 <scx567888@outlook.com>
2026-02-05 13:43:00 +0800

    v0.3.1

--- src/main/java/dev/scx/http/media/byte_input/ByteInputMediaWriter.java
+++ src/main/java/dev/scx/http/media/byte_input/ByteInputMediaWriter.java
@@ -12,21 +12,16 @@ import dev.scx.io.exception.ScxOutputException;
 
 /// ByteInputMediaWriter
 ///
+/// 默认自动关闭输入流
+///
 /// @author scx567888
 /// @version 0.0.1
 public final class ByteInputMediaWriter implements MediaWriter {
 
     private final ByteInput byteInput;
-    private final boolean autoClose;
 
     public ByteInputMediaWriter(ByteInput byteInput) {
-        // 直接传输的时候 一般表示 整个输入流已经被用尽了 所以这里我们默认自动关闭输入流.
-        this(byteInput, true);
-    }
-
-    public ByteInputMediaWriter(ByteInput byteInput, boolean autoClose) {
         this.byteInput = byteInput;
-        this.autoClose = autoClose;
     }
 
     @Override
@@ -42,15 +37,9 @@ public final class ByteInputMediaWriter implements MediaWriter {
 
     @Override
     public void write(ByteOutput byteOutput) throws ScxOutputException, OutputAlreadyClosedException, ScxInputException, InputAlreadyClosedException {
-        try (byteOutput) {
+        try (byteOutput; byteInput) {
             // 这里发生的 ScxInputException, InputAlreadyClosedException 错误我们直接穿透.
             ScxIO.transferToAll(byteInput, byteOutput);
-        }
-        // 不在 finally 中关闭:
-        // 仅当写入流程完整成功(包括 ByteOutput 关闭阶段)且 autoClose=true 时才关闭 byteInput.
-        // 若写入过程中抛出异常, 将保留 byteInput 由调用方自行处理.
-        if (autoClose) {
-            byteInput.close();
         }
     }
 
